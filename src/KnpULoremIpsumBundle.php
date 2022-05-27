45abb816868d56fe5be6d1abfde5cd8b14c8daf3
Tac Tacelosky <tacman@gmail.com>
2022-05-27 09:05:49 -0400

    working again

--- src/KnpULoremIpsumBundle.php
+++ src/KnpULoremIpsumBundle.php
@@ -2,18 +2,30 @@
 
 namespace KnpU\LoremIpsumBundle;
 
+use KnpU\LoremIpsumBundle\DependencyInjection\Compiler\WordProviderCompilerPass;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KnpULoremIpsumBundle extends AbstractBundle
 {
+//    public function build(ContainerBuilder $container)
+//    {
+//        $container->addCompilerPass(new WordProviderCompilerPass());
+//    }
+
     protected string $extensionAlias = 'knpu_lorem_ipsum';
 
     // $config is the bundle Configuration that you usually process in ExtensionInterface::load() but already merged and processed
-    /** @param array<mixed> $config */
+    /**
+     * @param array<mixed> $config
+     */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
 //        $loader = new XmlFileLoader($builder, new FileLocator(__DIR__.'/Resources/config'));
